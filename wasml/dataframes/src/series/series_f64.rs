d167d481a8c81fdf6c4b8226d818c5adb5b3ac1d
vedpatil611 <vedpatil611@gmail.com>
2021-04-28 22:31:46 +0530

    float series methods and minor optimizations

--- wasml/dataframes/src/series/series_f64.rs
+++ wasml/dataframes/src/series/series_f64.rs
@@ -1,7 +1,9 @@
 use ndarrays::one_dimensional::floats::Floats1d;
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
 pub struct SeriesF64 {
     name: String,
     data: Floats1d,
@@ -16,48 +18,46 @@ impl SeriesF64 {
         let serde_data: Vec<f64> = serde_wasm_bindgen::from_value(data).unwrap();
         let col_data = Floats1d::new(serde_data);
         let col_size = col_data.len();
-        let new_series = SeriesF64 {
+        SeriesF64 {
             name: col_name,
             data: col_data,
             size: col_size,
-        };
-
-        new_series
+        }
     }
-    pub fn show(&self) -> String {
-        let margin = "#".repeat(self.size + 100);
-        let col_name = self.name.clone();
-        let col_size = self.size.to_string();
-        let mut c = 0;
-        let data: String = self
-            .data
-            .vector_data()
-            .iter()
-            .map(|&x| {
-                c += 1;
-                if c == self.size {
-                    x.to_string()
-                } else {
-                    x.to_string() + ", "
-                }
-            })
-            .collect();
-        format!(
-            "{}\nName: {}\nData: {}\n(1x{})\n{}",
-            margin, col_name, data, col_size, margin
-        )
+
+    pub fn show(&self) -> JsValue {
+        serde_wasm_bindgen::to_value(&self).unwrap()
     }
 
     pub fn data(&self) -> JsValue {
         self.data.data_to_js()
     }
 
-    // pub fn push_data(&mut self, data_item: JsValue) -> js_sys::Float64Array {
-    //     let data_item = serde_wasm_bindgen::from_value(data_item).unwrap();
-    //     self.data.push(data_item);
-    //     self.size += 1;
-    //     return js_sys::Float64Array::from(&self.data[..]);
-    // }
+    pub fn append(&mut self, data_item: JsValue) {
+        let data_item = serde_wasm_bindgen::from_value(data_item).unwrap();
+        self.data.append(data_item);
+    }
+
+    pub fn appended(&mut self, data_item: JsValue) -> JsValue {
+        let data_item = serde_wasm_bindgen::from_value(data_item).unwrap();
+        self.data.append(data_item);
+
+        self.data.data_to_js()
+    }
+
+    pub fn extend(&mut self, data_arr: JsValue) {
+        let data_arr = serde_wasm_bindgen::from_value(data_arr).unwrap();
+        let ndarray_data_arr = Floats1d::new(data_arr);
+        self.data.extend(ndarray_data_arr)
+    }
+
+    pub fn extended(&mut self, data_arr: JsValue) -> JsValue {
+        let data_arr = serde_wasm_bindgen::from_value(data_arr).unwrap();
+        let ndarray_data_arr = Floats1d::new(data_arr);
+        self.data.extend(ndarray_data_arr);
+
+        self.data.data_to_js()
+    }
 
     pub fn name(&self) -> String {
         self.name.clone()
@@ -71,6 +71,6 @@ impl SeriesF64 {
     }
 
     pub fn size(&self) -> usize {
-        self.size.clone()
+        self.size
     }
 }
