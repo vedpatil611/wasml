873adeadccb699202f0ad0ea2f813060c137ec86
Dion Pinto <dionpinto000@gmail.com>
2021-05-12 17:45:36 +0530

    🔨index.js fix

--- wasml/dataframes/web/index.js
+++ wasml/dataframes/web/index.js
@@ -8,36 +8,18 @@ import init, {
 (async () => {
   await init();
 
-  let ser1 = new SeriesI32("Dion", [1, 2]);
-  let ref_ser1 = ser1.toJson();
-  console.log("################--SERIES (Int)--##################");
-  console.table(ser1.display);
-  console.log("################--SERIES DATA--##################");
-  console.table(ser1.data());
+  let ser1 = new SeriesI32("Integer", [1, 2]);
 
-  let ser2 = new SeriesI32("Pinto", [6, 7]);
-  let ref_ser2 = ser2.toJson();
-  console.log("################--SERIES (Float)--##################");
-  const float_data = new SeriesF64("Temp", [12, 12.9]);
-  const float_data2 = new SeriesF64("Temp2", [9.99, 9.1]);
+  const float_data = new SeriesF64("Float", [12, 12.9]);
 
-  float_data.extend([11.1, 11.1]);
-
-  float_data2.extend([11.1, 11.1]);
-
-  console.table(float_data.display);
-  console.table(float_data2.display);
-
-  let dataf = new DataFrame([ser1.get_ref(), float_data2.get_ref()]);
-
-  let dataf = new DataFrame([ref_ser1, float_data2.toJson(), ser2.toJson()]);
-  console.log("################--DF--##################");
-  console.log(dataf.show());
-  dataf.append(ColumnType.FLOAT, float_data.toJson());
+  let dataf = new DataFrame([ser1.toJson(), float_data.toJson()]);
   console.log("################--DF--##################");
   console.log(dataf.display);
+  // dataf.append(ColumnType.FLOAT, float_data.toJson());
+  // console.log("################--DF--##################");
+  // console.log(dataf.display);
 
-  console.log(dataf.size());
-  console.log(dataf.columns());
-  console.log(dataf.dTypes);
+  // console.log(dataf.size());
+  // console.log(dataf.columns());
+  // console.log(dataf.dTypes);
 })();
